2715289c5169e6ad6e02a48a97672637a50c5964
Ashley Anderton <ashley.anderton@civil.lmco.com>
2018-12-06 11:57:30 +0000

    Changed name to reflect wider service pattern use, removed breadcrumb example

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Explore careers - Snippets',
+  serviceName: 'DFC - Patterns',
 
   // Default port that prototype runs on
   port: '3000',
